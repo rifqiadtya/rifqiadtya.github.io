ab37d7b6050b6847c5a0ed9f4dfb0ceb3cd732d9
rifqiadtya <1811501517@student.budiluhur.ac.id>
2021-05-27 15:08:14 +0700

    andai

--- pages/home.php
+++
@@ -1,8 +0,0 @@
-<?
-
-
-?>
-<body style="background: url('https://source.unsplash.com/G8rRItjrwkA') no-repeat center center fixed; background-size: cover">
-<h1 style="text-align: center; padding-top: 240px; color:seashell; font-size: 3rem; letter-spacing: 1px">Welcome home, <br> <?php echo $_SESSION['userData']['fullname']; ?></h1>
-<h4 style="text-align: center; padding-top: 2px; font-size: 1.3rem; letter-spacing: 1px"><a href="index.php?act=devices"> Get Started Now </a></h4>
-</body>
